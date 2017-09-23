cac713c5eaec486b69960d2b777bb0da03a99b60
TrueBrain <truebrain@openttd.org>
2017-09-23 05:29:52 -0400

    Fix: typo in build.sh prevented building binaries

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ for c in generated/*; do
 	rm -rf $c/workdir
 	mkdir -p $c/workdir
 	cp -r source $c/workdir/source
-	docker run --id=`id -u`:`id -g` --mount type=bind,src=$(realpath $c/workdir),target=/workdir openttd-cf:$(basename ${c})
+	docker run --user=`id -u`:`id -g` --mount type=bind,src=$(realpath $c/workdir),target=/workdir openttd-cf:$(basename ${c})
 	cp $c/workdir/source/bundles/* binaries
 done
 
