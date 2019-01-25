9622757c5040f98d4875add1a1f3628ddfd9bc01
Patric Stout <truebrain@openttd.org>
2019-01-25 14:02:32 +0100

    Add: also create 'ai' and 'gs' documentation tarballs

--- release-docs/files/run.sh
+++ release-docs/files/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 if [ -z "$1" ]; then
     echo "Usage: $0 <version>"
@@ -18,7 +18,17 @@ echo ""
 mkdir -p objs
 doxygen
 
-mkdir -p bundles
-mv docs/source ${BASENAME}-docs
+(
+    cd src/script/api
+    doxygen Doxyfile_AI
+    doxygen Doxyfile_Game
+)
 
-tar --xz -cf bundles/${BASENAME}-docs.tar.xz ${BASENAME}-docs
+mv docs/source ${BASENAME}-docs-source
+mv docs/aidocs ${BASENAME}-docs-ai
+mv docs/gamedocs ${BASENAME}-docs-gs
+
+mkdir -p bundles
+tar --xz -cf bundles/${BASENAME}-docs-source.tar.xz ${BASENAME}-docs-source
+tar --xz -cf bundles/${BASENAME}-docs-ai.tar.xz ${BASENAME}-docs-ai
+tar --xz -cf bundles/${BASENAME}-docs-gs.tar.xz ${BASENAME}-docs-gs
