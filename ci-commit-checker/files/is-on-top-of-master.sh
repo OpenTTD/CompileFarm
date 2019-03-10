853ad178b209bcd5f716f361351eebb24301b065
glx <glx@openttd.org>
2019-03-10 01:59:42 +0100

    Fix #29: don't assume master as target branch

--- ci-commit-checker/files/is-on-top-of-master.sh
+++ ci-commit-checker/files/is-on-top-of-master.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-is_on_top_of_master=$(git branch $(git rev-parse --abbrev-ref HEAD) --contains origin/master | wc -l)
+if [ -z "${TARGET_BRANCH}" ]; then
+	TARGET_BRANCH=master
+fi
+
+is_on_top_of_master=$(git branch $(git rev-parse --abbrev-ref HEAD) --contains origin/${TARGET_BRANCH} | wc -l)
 
 if [ "${is_on_top_of_master}" -ne 1 ]; then
-    echo "ERROR: This commit is not on top of master"
+    echo "ERROR: This commit is not on top of ${TARGET_BRANCH}"
     echo "(no further validation is done due to this error)"
     exit 1
 fi
